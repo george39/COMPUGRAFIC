2d99e9b7f1a321b2927392ae653d8bb2b81c2d66
GEORGE <george35@utp.edu.co>
2016-12-11 20:15:52 -0500

    pasar al nivel3

--- juego.py
+++ juego.py
@@ -189,6 +189,105 @@ class jugador(pygame.sprite.Sprite):
         """ Usuario no pulsa teclas """
         self.var_x = 0 
 
+class jugador3(pygame.sprite.Sprite):
+    lp=None
+    la=None
+    def __init__(self, img_sprite):
+        pygame.sprite.Sprite.__init__(self)
+        '''
+        self.image=pygame.Surface([30,50])
+        self.image.fill(VERDE)
+        self.rect=self.image.get_rect()
+        self.var_x=0
+        self.var_y=0
+        '''
+        self.image = img_sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+        self.var_x = 0
+        self.var_y = 0
+        self.con = 0
+        self.dir = 0
+        self.vida = 1
+    def gravedad(self):
+        
+        if self.var_y==0:
+            self.var_y=1
+        else:
+            self.var_y+=0.35
+
+        
+        
+        # Detener movimiento vertical
+           
+        '''
+        if self.rect.y >= ALTO - self.rect.height and self.var_y >= 0:
+            self.var_y = 0
+            self.rect.y = ALTO - self.rect.height    
+        '''
+        if self.rect.y >= ALTO-18-self.rect.height and self.var_y >=0:
+            self.rect.bottom=ALTO-18
+            self.var_y=0
+          
+     
+    def update(self):
+        self.vida
+        self.rect.x += self.var_x
+        self.rect.y += self.var_y
+        if self.con < 4:
+            self.con += 1
+        else:
+            self.con = 0
+
+        self.gravedad()
+        self.rect.x+=self.var_x
+        '''
+        l_col=pygame.sprite.spritecollide(self,self.lp,False)
+        for pl in l_col:
+            if self.var_x>0:
+                self.rect.right=ANCHO#pl.rect.left
+            else:
+                self.rect.left=pl.rect.right
+        
+        '''
+               
+        self.rect.y+=self.var_y
+        '''
+        l_col=pygame.sprite.spritecollide(self,self.lp,False)
+        for pl in l_col:
+            if self.var_y>0:
+                self.rect.bottom=pl.rect.top
+                if pl.var_y!=0:
+                    self.rect.y+=pl.var_y
+            else:
+                self.rect.top=pl.rect.bottom
+                if pl.var_y!=0:
+                    self.rect.y-=pl.var_y
+            self.var_y=0
+        '''
+        '''
+        #CHOQUES CON LOS BORDES
+        if self.rect.right>100:
+            self.rect.right=100
+
+
+        if self.rect.left<100:
+            self.rect.left=100
+            #self.var_x=0        
+        '''
+        if self.rect.right>ANCHO:
+            self.rect.right=ANCHO
+            self.var_x=0
+
+        if self.rect.left<100:
+            self.rect.left=100
+            self.var_x=0
+              
+    def no_mover(self):
+        """ Usuario no pulsa teclas """
+        self.var_x = 0 
+
 class enemigo(pygame.sprite.Sprite):
     bloques = None
     def __init__(self, archivo):
@@ -209,6 +308,53 @@ class enemigo(pygame.sprite.Sprite):
         if self.tiempo==0:
             self.disparar=True
             self.tiempo=random.randrange(20)
+
+
+class enemigopatron(pygame.sprite.Sprite):
+    bloques = None
+    def __init__(self, archivo):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(archivo).convert_alpha()
+        self.rect=self.image.get_rect()
+        self.rect.x=0#ANCHO
+        self.rect.y=0#ALTO
+        self.var_x=0
+        self.var_y=0
+        self.disparar=False
+        self.vida=3
+        self.tiempo=random.randrange(20)
+    def update(self):
+        self.rect.x+=self.var_x
+        self.rect.y+=self.var_y
+        self.tiempo-=1
+        if self.tiempo==0:
+            self.disparar=True
+            self.tiempo=random.randrange(20) 
+
+        #limites
+        ''' 
+        if self.rect.right>ANCHO-150:
+            #self.rect.right=ANCHO-150
+            self.var_x-=2
+
+        if self.rect.left<150:
+            #self.rect.left=150
+            self.var_x+=2
+        
+        if self.rect.top<0:
+            #self.rect.top=100 
+            self.var_y+=2 
+        if self.rect.bottom==300:
+            self.var_x-=2
+            self.var_y=0
+        
+        if self.rect.left==100:
+            self.var_y-=2
+            self.var_x=0
+            #self.rect.bottom=ALTO-400
+        '''    
+
+                     
 
 
 #CLASE PARA CREAR LAS BALAS
@@ -330,11 +476,21 @@ def pausa():
                 elif event.key == pygame.K_q:
                     pygame.quit()
                     quit()
+
         pantalla.fill(NEGRO)
         texto=fuente.render("PAUSADO", True, BLANCO)
         pantalla.blit(texto,(100,100))
         texto=fuente.render("presione P para continuar o Q para terminar", True, BLANCO)
         pantalla.blit(texto,(100,400))
+        pygame.display.update()            
+        '''            
+        pantalla.fill(NEGRO)
+        
+        texto=fuente.render("PAUSADO", True, BLANCO)
+        pantalla.blit(texto,(100,100))
+        texto=fuente.render("presione P para continuar o Q para terminar", True, BLANCO)
+        pantalla.blit(texto,(100,400))
+        '''
         pygame.display.update()
         #reloj.tick(2) 
 
@@ -461,7 +617,7 @@ if __name__ == '__main__':
     j2=Jugador_nivel2(animal2[4][7])
     todos2.add(j2)
     jugadores2.add(j2)
-    
+    '''
     #ENEMIGOS HORIZONTALES 
     for i in range(30):
         en2=enemigo('img/pirana1.png')
@@ -481,24 +637,46 @@ if __name__ == '__main__':
         en3.var_x=random.randrange(2,4)
         enemigos2.add(en3)
         todos2.add(en3) 
-    
+    '''
     #ENEMIGO PATRON
     for i in range(1):
-        en4=enemigo('img/pantano.png')
+        en4=enemigopatron('img/pantano.png')
         en4.rect.x=random.randrange(100,ANCHO-100)
-        en4.rect.y=random.randrange(-30,300)
+        en4.rect.y=0#random.randrange(-30,300)
         #en.var_x=(-1)*random.randrange(1,10)
         en4.var_y=random.randrange(2,4)
         enemigos2.add(en4)
         todos2.add(en4)        
       
     #********************************************************************
+    #********************************************************************
+    #          NIVEL 3
+    #********************************************************************
+    fondo3=pygame.image.load('img/im3.jpeg')
+    dim_fondo3=fondo3.get_rect()
+    ventana3=fondo3.subsurface(3488,0,ANCHO,ALTO)
+
+    todos3=pygame.sprite.Group()
+    enemigo3=pygame.sprite.Group()
+    jugadores3=pygame.sprite.Group()
+
+    animal3 = Recortar('img/soldier.png', 32,32) 
+    j3=jugador3(animal3[4][7])
+    todos3.add(j3)
+    jugadores3.add(j3) 
+
+
+    #********************************************************************
     var_x=0
     pos_x=0
     var_y=0
     pos_y=0
+    var_y2=0
+    pos_y2=0
     conenemi=50
     nivel =0
+    nivel2=1
+    nivel3=0
     enemimuerots = 10
 
     
@@ -511,6 +689,7 @@ if __name__ == '__main__':
     seleccionar="play"
     pag=0
     terminar=False
+
     while not terminar:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -532,7 +711,7 @@ if __name__ == '__main__':
                 
           
 
-            
+            #menu pausa
             if event.type==pygame.KEYDOWN:
                 if event.key == pygame.K_p:
                     pausa()
@@ -544,6 +723,10 @@ if __name__ == '__main__':
                     j2.var_x=2
                     j2.var_y=0
                     j2.dir=2
+
+                    j3.var_x=2
+                    j3.var_y=0
+                    j3.dir=2
                    
                     #b.var_x=-3
                 if event.key==pygame.K_LEFT:
@@ -554,10 +737,20 @@ if __name__ == '__main__':
                     j2.var_x=-2
                     j2.var_y=0
                     j2.dir=1
+
+
+                    j3.var_x=-2
+                    j3.var_y=0
+                    j3.dir=1
+
+
+                    '''
                     arriba = False
                     abajo = False
                     izquierda = True
+                    
                     derecha = False
+                    '''
                 if event.key==pygame.K_UP:
                     jp.var_y=-8
                     jp.rect.y += -2
@@ -565,6 +758,11 @@ if __name__ == '__main__':
                     j2.var_y=-2
                     j2.var_x=0
                     j2.dir=3
+
+                    j3.var_y=-8
+                    j3.rect.y += -2
+                    j3.var_x=0
+                        
                     arriba = True
                     abajo = False
                     izquierda = False
@@ -574,6 +772,9 @@ if __name__ == '__main__':
                     j2.var_y=2
                     j2.var_x=0
                     j2.dir=0
+
+                   
+                    
                     arriba = False
                     abajo = True
                     izquierda = False
@@ -621,6 +822,21 @@ if __name__ == '__main__':
                 if event.key == pygame.K_DOWN and j2.var_y > 0:
                         
                         j2.no_mover2()
+                
+                #no mover 3
+                if event.key==pygame.K_SPACE:
+                    #balas.remove(b)
+                    todos.remove(b)
+                    be.dir=9
+                if event.key == pygame.K_LEFT and j3.var_x < 0:
+                    j3.no_mover()
+
+                    #j2.no_mover2()
+
+                if event.key == pygame.K_RIGHT and j3.var_x > 0:
+                        j3.no_mover()
+                           
+                
                 
         #ELIMINAR AL ENEMIGO
 
@@ -765,6 +981,28 @@ if __name__ == '__main__':
                     jugadores.remove(im)
                     todos.remove(im)            
                 '''
+
+        #LIMITES DEL PATRON        
+        if en4.rect.left<0:
+            en4.var_y=5
+            en4.var_x=0 
+
+        if en4.rect.bottom>ALTO-200:
+            en4.var_x=5
+            en4.var_y=0 
+
+        if en4.rect.right>ANCHO:#-en.rect.right:
+            en4.var_y=-5
+            en4.var_x=0             
+        
+        if en4.rect.top<0:
+            en4.var_x=-5
+            en4.var_y=0      
+            if en4.rect.left<0:
+                en4.var_y=5
+                en4.var_x=0    
+
+
         #ELIMINAR BALAS QUE SALGAN DE LA PANTALLA 
         for eb in balas2:
             if eb.rect.x < 0 or eb.rect.x > ANCHO or eb.rect.y > ALTO or eb.rect.y<0:
@@ -784,17 +1022,29 @@ if __name__ == '__main__':
         else:
             j2.image=animal2[1+j2.con][j2.dir] 
 
+        
+
+        #ANIMACION DEL JUGADOR 3
+        if j3.var_x==0:
+            j3.image=animal3[0][j3.dir]
+        else:
+            j3.image=animal3[0+j3.con][j3.dir]
 
 
-
-        #ATAJO PARA PASAR MUNDO
+        #ATAJO PARA PASAR MUNDO 1
         if jp.rect.top==blo.rect.bottom:
             #pos_y=1
             pos_x=(dim_fondo.width-ANCHO)
             ventana=fondo.subsurface(pos_x,2248,ANCHO,ALTO)
 
         
-        #MOVIMIENTO DEL FONDO
+        #ATAJO PARA PASAR MUNDO 2
+        if j2.rect.right==ANCHO-100:
+            #pos_y=1
+            pos_x=(dim_fondo2.height-ALTO)
+            ventana2=fondo2.subsurface(3488,pos_x,ANCHO,ALTO) 
+
+        #MOVIMIENTO DEL FONDO 1
         if jp.var_x >0 and pos_x < (dim_fondo.width - ANCHO):
             pos_x+=2
         if jp.var_x<0 :
@@ -809,20 +1059,49 @@ if __name__ == '__main__':
         if j2.var_y<0 :
             pos_y-=2
         if pos_y>0 and pos_y < (dim_fondo2.height - ALTO):
-            ventana2=fondo2.subsurface(3488,pos_y,ANCHO,ALTO)    
+            ventana2=fondo2.subsurface(3488,pos_y,ANCHO,ALTO)
+
+        
+        #MOVIMIENTO DEL FONDO 3
+        if j3.var_x >0 and pos_x < (dim_fondo3.width - ANCHO):
+            pos_y2+=2
+
+        if j3.var_x<0 :
+            pos_y2-=2
+
+        if pos_x>0 and pos_x < (dim_fondo3.width - ANCHO):
+            ventana3=fondo3.subsurface(pos_y2,2248,ANCHO,ALTO)
+        
+
+        
 
         #LLAMAR SEGUNDO NIVEL
-
+        
         if pos_x>0 and pos_x==(dim_fondo.width-ANCHO):
-
+            
             jp.var_y-=1
             nivel+=1
+            nivel2=1
+            
+            print "nivel 2",nivel2
+            print "nivel",nivel
             #j2.vida=jp.vida
             todos.remove(en)
             jugadores.remove(en)
+           
+
+        #LLAMAR NIVEL 3
+
+        if pos_x>0 and pos_x==(dim_fondo2.height-ALTO):
+            nivel2+=1
+            #j3.var_y-=1
+            #nivel+=1
+            #j2.vida=jp.vida
+            todos2.remove(en4)
+            jugadores2.remove(en4)    
 
 
-        
+        #CUANDO EL JUGADOR PIERDE EN EL PRIMER NIVEL
         if jp.vida <1 and nivel==0:
                 
             pantalla.fill(NEGRO)
@@ -831,6 +1110,8 @@ if __name__ == '__main__':
             #imagen=pygame.image.load('img/kameha.png')
             #pantalla.blit(imagen,[120,150])
             pygame.display.flip()
+           
+
 
          
               
@@ -857,7 +1138,7 @@ if __name__ == '__main__':
             pygame.display.flip()
             reloj.tick(60)
 
-      
+        #CUANDO EL JUGADOR PIERDE EN SEGUNDO NIVEL
         if j2.vida <1 :
                 
                 pantalla.fill(NEGRO)
@@ -866,12 +1147,23 @@ if __name__ == '__main__':
                 imagen=pygame.image.load('img/kameha.png')
                 pantalla.blit(imagen,[120,150])
                 pygame.display.flip()
-
-        if nivel>1 and j2.vida>0:
-            
+        
+        #ACTUALIZAR PANTALLA SEGUNDO NIVEL
+        if nivel>1 and j2.vida>0 and nivel2==1:
+            nivel3+=1
+            print "suma nivel",nivel
             pantalla.blit(ventana2,(0,0))
             todos2.update()
             todos2.draw(pantalla)
             pygame.display.flip()
             reloj.tick(60)
+        
+        #ACTUALIZAR PANTALLA  NIVEL 3
+        if nivel2>1 and nivel3>1:
+            #print "nivel",nivel
+            pantalla.blit(ventana3,(0,0))
+            todos3.update()
+            todos3.draw(pantalla)
+            pygame.display.flip()
+            reloj.tick(60)    
 
